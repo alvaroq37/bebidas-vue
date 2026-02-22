dd6e028b551ad0fbb6a4547ac63c8e760d6d7aeb
alvaroq37 <alvaro_q23@hotmail.com>
2026-02-22 19:09:26 -0400

    # Se agrega el componente de Receta para visualizar los resultados

---
+++ src/components/RecetaView.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+  receta: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="rounded-lg shadow">
+    <div class="overflow-hidden">
+      <img
+        :src="receta.strDrinkThumb"
+        :alt="'Imagen de ' + receta.strDrink"
+        class="hover:scale-125 transition-transform hover:rotate-2"
+      />
+    </div>
+    <div class="p-5">
+      <h2 class="text-2xl truncate font-extrabold">{{ receta.strDrink }}</h2>
+      <button
+        type="button"
+        class="bg-orange-400 text-white uppercase font-bold w-full mt-5 p-3 rounded-lg hover:bg-orange-500 transition-colors cursor-pointer"
+      >
+        Ver Receta
+      </button>
+    </div>
+  </div>
+</template>
---
+++ src/service/APIService.js
@@ -0,0 +1,18 @@
+import api from '../lib/axios'
+
+export default {
+  async obtenerCategorias() {
+    try {
+      return await api.get('/list.php?c=list')
+    } catch (error) {
+      console.error('Error fetching categories:', error)
+    }
+  },
+  async obtenerRecetas({ categoria, nombre }) {
+    try {
+      return await api.get(`/filter.php?c=${categoria}&i=${nombre}`)
+    } catch (error) {
+      console.error('Error fetching recipes:', error)
+    }
+  },
+}
